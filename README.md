64ef110a6939508b582296f6cc858e76dd2f4760
juzhenxing <juzhenxing@shein.com>
2021-07-17 15:09:30 +0800

    mysql8.0.22是可以使用5.1.44 jdbc的

---
+++ README.md
@@ -0,0 +1,9 @@
+# mapper生成器
+
+#### 介绍
+1. 引入驱动依赖
+2. 修改配置文件
+3. 控制台执行命令
+```
+java -jar mybatis-generator-core-1.3.7.jar -configfile generatorConfig.xml
+```
---
+++ com/example/model/StyleChildQuotaHistoryEntity.java
@@ -0,0 +1,135 @@
+package model;
+
+import java.util.Date;
+
+public class StyleChildQuotaHistoryEntity {
+    private Integer id;
+
+    private Integer supplierId;
+
+    private Date operationDate;
+
+    private Byte type;
+
+    private Byte isDelete;
+
+    private String addUid;
+
+    private Date addTime;
+
+    private String updateUid;
+
+    private Date updateTime;
+
+    private Date lastUpdateTime;
+
+    private Long quotaUsed;
+
+    private Long attributeValueId;
+
+    private Long categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Date getOperationDate() {
+        return operationDate;
+    }
+
+    public void setOperationDate(Date operationDate) {
+        this.operationDate = operationDate;
+    }
+
+    public Byte getType() {
+        return type;
+    }
+
+    public void setType(Byte type) {
+        this.type = type;
+    }
+
+    public Byte getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Byte isDelete) {
+        this.isDelete = isDelete;
+    }
+
+    public String getAddUid() {
+        return addUid;
+    }
+
+    public void setAddUid(String addUid) {
+        this.addUid = addUid;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public String getUpdateUid() {
+        return updateUid;
+    }
+
+    public void setUpdateUid(String updateUid) {
+        this.updateUid = updateUid;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+    public Long getQuotaUsed() {
+        return quotaUsed;
+    }
+
+    public void setQuotaUsed(Long quotaUsed) {
+        this.quotaUsed = quotaUsed;
+    }
+
+    public Long getAttributeValueId() {
+        return attributeValueId;
+    }
+
+    public void setAttributeValueId(Long attributeValueId) {
+        this.attributeValueId = attributeValueId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+}
